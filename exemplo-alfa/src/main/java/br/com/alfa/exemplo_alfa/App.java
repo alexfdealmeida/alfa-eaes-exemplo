2d367d5b5932303123c924e8f2432256a99f57ea
alexfdealmeida <alexferreiradealmeida@gmail.com>
2017-11-11 16:30:45 -0200

    novo teste compilação codeship

--- exemplo-alfa/src/main/java/br/com/alfa/exemplo_alfa/App.java
+++ exemplo-alfa/src/main/java/br/com/alfa/exemplo_alfa/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World - first example!" );
+        System.out.println( "Hello World!" );
     }
 }
